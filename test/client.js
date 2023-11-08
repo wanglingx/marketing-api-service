5330b325d1326f0860ff871c39c31c369cac0242
Jialing Wang <annisanst62@gmail.com>
2023-11-08 17:13:57 +0700

    update code

--- test/client.js
+++ test/client.js
@@ -1,23 +1,24 @@
 const soap = require('soap');
 
 // Create the SOAP client
-const url = 'http://localhost:8000/weather?wsdl';
-soap.createClient(url, function(err, client) {
+const url = 'http://localhost:3000/shipping?wsdl';
+
+soap.createClient(url, function (err, client) {
   if (err) {
     console.error('Error creating SOAP client:', err);
     return;
   }
 
   // Make a SOAP request
-  const args = { location: 'New York' };
-  client.GetWeather(args, function(err, result) {
+  const args = { Shipping_cost: 3 };
+
+  client.shippingCost(args, function (err, result) {
     if (err) {
       console.error('Error making SOAP request:', err);
       return;
     }
 
-    // Handle the SOAP response
-    console.log('Temperature:', result.temperature);
-    console.log('Description:', result.description);
+    // Access the result from the SOAP response
+    console.log('SOAP Response:', result);
   });
 });
