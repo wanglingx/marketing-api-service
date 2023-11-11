6afd14a913d54021b5e64bb0791d819a2cd16fab
Jialing Wang <annisanst62@gmail.com>
2023-11-11 16:49:30 +0700

    remove db connention

--- config/connector.js
+++ config/connector.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://root:1234@cluster0.h4iejmb.mongodb.net/Marketing-xml', {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
